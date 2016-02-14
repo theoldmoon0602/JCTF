cf0091c837891fbd8d5cea4f442f05404d092832
theoldmoon0602 <theoldmoon0602@gmail.com>
2016-02-14 22:46:02 +0900

    Created New Model User a CTF Player.

--- jctf/scoreboard/models.py
+++ jctf/scoreboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  """ User Model. The User is CTF Player """
+  name = models.CharField(max_length=140)
+  email = models.EmailField(unique=False)
+
+  def __str__(self):
+    return self.name
+
+
