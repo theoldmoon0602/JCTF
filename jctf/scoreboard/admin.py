c826402804e53e0ab1f66234d71fa95b19db1e55
theoldmoon0602 <theoldmoon0602@gmail.com>
2016-02-26 07:11:27 +0900

    Create Probelm and Hint modes

--- jctf/scoreboard/admin.py
+++ jctf/scoreboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Problem, Hint
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Problem)
+admin.site.register(Hint)
+
--- jctf/scoreboard/models.py
+++ jctf/scoreboard/models.py
@@ -1,11 +1,28 @@
 from django.db import models
+from django.core.validators import URLValidator
 
 class User(models.Model):
   """ User Model. The User is CTF Player """
-  name = models.CharField(max_length=140)
+  name = models.CharField(max_length=140, unique=True)
   email = models.EmailField(unique=False)
 
   def __str__(self):
     return self.name
 
+class Hint(models.Model):
+  name = models.CharField(max_length=128, default='')
+  text = models.TextField()
+  order = models.IntegerField(default=0)
 
+  def __str__(self):
+    return self.text
+
+class Problem(models.Model):
+  name = models.CharField(max_length=256, unique=True)
+  text = models.TextField()
+  url = models.TextField(validators=[URLValidator()])
+  solvedby = models.ManyToManyField(User)
+  hint = models.ManyToManyField(Hint)
+
+  def __str__(self):
+    return self.name
