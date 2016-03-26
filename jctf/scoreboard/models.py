a95dc1a33965bae316ad5af551f7b79632fca3b6
theoldmoon0602 <theoldmoon0602@gmail.com>
2016-03-27 00:02:35 +0900

    hint is shown by name, problem's some property allow blank

--- jctf/scoreboard/models.py
+++ jctf/scoreboard/models.py
@@ -1,28 +1,31 @@
 from django.db import models
 from django.core.validators import URLValidator
 
-class User(models.Model):
-  """ User Model. The User is CTF Player """
-  name = models.CharField(max_length=140, unique=True)
-  email = models.EmailField(unique=False)
 
-  def __str__(self):
-    return self.name
+class User(models.Model):
+    """ User Model. The User is CTF Player """
+    name = models.CharField(max_length=140, unique=True)
+    email = models.EmailField(unique=False)
+
+    def __str__(self):
+        return self.name
+
 
 class Hint(models.Model):
-  name = models.CharField(max_length=128, default='')
-  text = models.TextField()
-  order = models.IntegerField(default=0)
+    name = models.CharField(max_length=128)
+    text = models.TextField()
+    order = models.IntegerField(default=0)
 
-  def __str__(self):
-    return self.text
+    def __str__(self):
+        return self.name
+
 
 class Problem(models.Model):
-  name = models.CharField(max_length=256, unique=True)
-  text = models.TextField()
-  url = models.TextField(validators=[URLValidator()])
-  solvedby = models.ManyToManyField(User)
-  hint = models.ManyToManyField(Hint)
+    name = models.CharField(max_length=256, unique=True)
+    text = models.TextField()
+    url = models.TextField(validators=[URLValidator()], blank=True)
+    solvedby = models.ManyToManyField(User, blank=True)
+    hint = models.ManyToManyField(Hint, blank=True)
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
