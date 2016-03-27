d433cac1990be985e421bcda03bb02383232e684
theoldmoon0602 <theoldmoon0602@gmail.com>
2016-03-27 11:03:06 +0900

    create scoreboard index router

--- jctf/scoreboard/views.py
+++ jctf/scoreboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Problem
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello world")
+    try:
+      ps = Problem.objects.all()
+    except e:
+     pass
+    return render(request, 'board.html', {'data': ps})
