b5f8c4868253551730c5bf8048c928424ae49e7a
theoldmoon0602 <theoldmoon0602@gmail.com>
2016-03-27 16:05:12 +0900

    hint - user relation created

--- jctf/scoreboard/models.py
+++ jctf/scoreboard/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from django.core.validators import URLValidator
 
-
-class User(models.Model):
-    """ User Model. The User is CTF Player """
-    name = models.CharField(max_length=140, unique=True)
-    email = models.EmailField(unique=False)
-
-    def __str__(self):
-        return self.name
-
-
 class Hint(models.Model):
     """ Hints are Belong to one Problem. """
     name = models.CharField(max_length=128)
@@ -21,10 +11,21 @@ class Hint(models.Model):
         return self.name
 
 
+class User(models.Model):
+    """ User Model. The User is CTF Player """
+    name = models.CharField(max_length=140, unique=True)
+    email = models.EmailField(unique=False)
+    used_hints = models.ManyToManyField(Hint, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Problem(models.Model):
     """ Problems. Containing problem text and ... """
     name = models.CharField(max_length=256, unique=True)
-    flag = models.CharField(max_length=256, unique=True, default='')
+    flag = models.CharField(max_length=256, unique=True)
+    point = models.IntegerField()
     text = models.TextField(blank=True,)
     url = models.TextField(validators=[URLValidator()], blank=True)
     solvedby = models.ManyToManyField(User, blank=True)
